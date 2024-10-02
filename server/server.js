be075cb4f17ec43be11a17bc46e48f5dd5047ea9
pericvladimir <vlado.peric8@gmail.com>
2024-10-02 19:07:44 +0200

    poz test

--- server/server.js
+++ server/server.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 
 console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
 
 app.listen(3000, (req, res) => {
   console.log("server running on port 3000");
