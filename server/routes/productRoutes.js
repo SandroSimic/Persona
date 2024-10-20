c664b971611bc56ed32ee55a8e3d2118690972d3
pericvladimir <vlado.peric8@gmail.com>
2024-10-20 21:13:22 +0200

    Backend finished v1

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -7,17 +7,19 @@ import {
   deleteProduct,
   getTopProducts,
 } from "../controllers/productController.js";
+import { protect, isUserAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").get(getAllProducts).post(createProduct);
+// protected and admin for create delete update
+router.route("/").get(getAllProducts).post(protect, isUserAdmin, createProduct);
 
 router.get("/get-top-products", getTopProducts);
 
 router
   .route("/:id")
   .get(getProductById)
-  .delete(deleteProduct)
-  .put(updateProduct);
+  .delete(protect, isUserAdmin, deleteProduct)
+  .put(protect, isUserAdmin, updateProduct);
 
 export default router;
